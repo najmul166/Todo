ac5984a62fcacb6fbcf0d90c220330340ec99ecd
najmul166 <2017100000051@seu.edu.bd>
2022-02-07 01:48:27 +0600

    secon

--- todo.js
+++ todo.js
@@ -39,19 +39,19 @@ function insertNewRecord(data) {
 }
 
 //Edit 
-//function onEdit(td) {
+function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
     document.getElementById("product").value = selectedRow.cells[1].innerHTML;
     document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
     document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
-//}
-//function updateRecord(formData) {
+}
+function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.productCode;
     selectedRow.cells[1].innerHTML = formData.product;
     selectedRow.cells[2].innerHTML = formData.qty;
     selectedRow.cells[3].innerHTML = formData.perPrice;
-//}
+}
 
 //Delete 
 function onDelete(td) {
